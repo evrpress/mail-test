ad53760c8dfc56841427f244154c6755ba8c2eba
Xaver <xaver@everpress.co>
2022-02-07 15:35:33 +0100

    first commit

---
+++ mail-test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Mail Test
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     mail-test
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Mail_Test
+ */
+
+// Your code starts here.
